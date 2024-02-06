7f50fe4028f5d36b597993fbea32438d435f387e
JanithNanayakkaraSE0612 <JanithNanayakkara0612@outlook.com>
2024-02-06 16:18:38 +0530

    Dashboard error fixing

--- my-app/src/components/DashboardView.tsx
+++ my-app/src/components/DashboardView.tsx
@@ -1,6 +1,7 @@
+'use client';
 import React, { useState } from 'react'
 import { FaSearch, FaEnvelope, FaRegBell } from "react-icons/fa"
-
+import profile from "../assets/profile.png"
 
 const Dashboardview = () => {
     const [open, setOpen] = useState(false)
@@ -17,7 +18,6 @@ const Dashboardview = () => {
                     <div className='bg-[#4E73DF] h-[40px] px-[14px] flex items-center justify-center cursor-pointer rounded-tr-[5px] rounded-br-[5px]'>
                         <FaSearch color='white' />
                     </div>
-
                 </div>
                 <div className='flex items-center gap-[20px]'>
                     <div className='flex items-center gap-[25px] border-r-[1px] pr-[25px]'>
@@ -27,10 +27,8 @@ const Dashboardview = () => {
                     <div className='flex items-center gap-[15px] relative' onClick={showProfile} >
                         <p>Douglas McGee</p>
                         <div className='h-[50px] w-[50px] rounded-full bg-[#4E73DF] cursor-pointer flex items-center justify-center relative z-40' >
-                            {/* <img src={profile} alt="" /> */}
-
+                            <img src={profile} alt="" />
                         </div>
-
                         {
                             open &&
                             <div className='bg-white border h-[120px] w-[150px] absolute bottom-[-135px] z-20 right-0 pt-[15px] pl-[15px] space-y-[10px]'>
@@ -38,11 +36,7 @@ const Dashboardview = () => {
                                 <p className='cursor-pointer hover:text-[blue] font-semibold'>Settings</p>
                                 <p className='cursor-pointer hover:text-[blue] font-semibold'>Log out</p>
                             </div>
-
                         }
-
-
-
                     </div>
                 </div>
             </div>
