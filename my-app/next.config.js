3b1a1f2096525e9dba70d0b4c4fc518457d0181a
JanithNanayakkaraSE0612 <JanithNanayakkara0612@outlook.com>
2024-02-06 18:43:45 +0530

    update

---
+++ my-app/next.config.js
@@ -0,0 +1,13 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {};
+
+// export default nextConfig;
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    experimental: {
+      typedRoutes: true,
+    },
+  }
+   
+  module.exports = nextConfig;
--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,22 +1,10 @@
-import SlideBar from "../components/SlideBar"
-import Dashboardview from "../components/DashboardView";
+import {Dashboard} from "../pages/Dashboard";
+
 
 export default function Home() {
   return (
     <div className="">
-    <div className="flex overflow-scroll ">
-      <div className="basis-[12%] h-[100vh]">
-        <SlideBar />
-      </div>
-      <div className="basis-[88%] border overflow-scroll h-[100vh]">
-          <Dashboardview /> 
-        <div>
-          {/* <Outlet></Outlet> */}
-        </div>
-      </div>
-
-
-    </div>
+        <Dashboard/>
   </div>
   );
 }
